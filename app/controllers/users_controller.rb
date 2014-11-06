5836849e707de20d62037f239d322eee2366df36
sandancient <sandancient@gmail.com>
2014-11-06 18:56:42 +0800

    add user email and phone

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :nickname, :student_id)
+    params.require(:user).permit(:name, :nickname, :student_id, :email, :phone)
   end
 
 end
